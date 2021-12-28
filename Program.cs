0c923cb160ca3f7167b6ac4167c860e45ec5d54b
Awesomefusion <alexmould@hotmail.co.uk>
2021-12-28 06:13:23 +0000

    First

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace TeacherApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int minStudents = 6;
+
+            Console.Write("Greetings teacher, please input number of students: ");
+            Console.WriteLine(" ");
+
+            int studentsInput = int.Parse(Console.ReadLine());
+
+            if (studentsInput >= minStudents)
+            {
+                Console.WriteLine("Number of students acceptable, thank you.");
+            }
+            else
+            {
+                Console.WriteLine("Please input a number of students of 6 or more.");
+                return;
+            }
+            List<Student> students = new List<Student>();
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Please enter Student Names and Scores.");
+
+            for (int i = 0; i < studentsInput; i++)
+            {
+                Student student = new Student();
+
+                Console.Write("Enter Student Name: ");
+                student.Name = Console.ReadLine();
+
+                Console.WriteLine(" ");
+
+                Console.Write("Enter Student Score: ");
+                student.Score = int.Parse(Console.ReadLine());
+
+                students.Add(student);
+            }
+
+            if (students.Count == studentsInput)
+            {
+                Console.WriteLine("Students list matches with initial inputted amount of students.");
+            }
+            else
+            {
+                Console.WriteLine("Students list doesn't match with initial inputted amount of students, please make sure you input data for each number of students selected.");
+                return;
+            }
+
+            foreach (var student in students)
+            {
+                if (student.Score < 40)
+                {
+                    student.Grade = "Fail";
+                }
+                else if (student.Score >= 40)
+                {
+                    if (student.Score <= 50)
+                    {
+                        student.Grade = "Pass";
+                    }
+                }
+                else if (student.Score >= 51)
+                {
+                    if (student.Score <= 69)
+                    {
+                        student.Grade = "Merit";
+                    }
+                }
+                else
+                {
+                    student.Grade = "Distinction";
+                }
+            }
+
+            List<Student> SortedList = students.OrderByDescending(o=>o.Grade).ToList();
+
+            using (StreamWriter sw = new StreamWriter("results.txt"))
+            {
+                foreach (var student in SortedList)
+                {
+                    sw.WriteLine(student.ToString());
+                }
+            }
+
+            Console.WriteLine("Thank you! All data is shown in the file results.txt");
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeacherApp
+{
+    public class Student
+    {
+     public string Name { get; set; }
+     public int Score { get; set; }
+     public string Grade { get; set; }
+    }
+}
